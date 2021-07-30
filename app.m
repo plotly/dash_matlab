2e7f1700f3a6d4f9c08ca4e0c433ee57043e6340
Jack Parmer <jparmer09@gmail.com>
2021-07-29 21:59:55 -0400

    Create app.m

---
+++ app.m
@@ -0,0 +1,34 @@
+% read data file
+
+tab = readtable('dat.csv');
+
+% create Dash app
+
+app = createApp()
+
+% create slider
+
+uifig = uifigure('visible', 'off');
+sld = uislider(uifig);
+dash_sld = ui2dash(sld, 'years')
+
+% create graph
+
+fig = figure('visible', 'off');
+YEAR = 1952;
+rows = (tab.year == YEAR);
+subtab = tab(rows,:);
+scatter(subtab.gdpPercap, ...
+    subtab.lifeExp, ...
+    subtab.pop/100000, ...
+    categorical(subtab.country), 'filled');
+ set(gca,'xscale','log');
+ fig = plotlyfig2dash(fig2plotly(gcf, 'offline', true));
+
+% add slider and graph to Dash app layout
+
+app = addLayout(app, fig, dash_sld)
+
+% run the app
+
+app.run_server(pyargs('debug',true,'use_reloader',false,'port','8057'))

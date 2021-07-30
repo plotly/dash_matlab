aac923804c19409eaeabd0448d92b9656e64485f
Jack Parmer <jparmer09@gmail.com>
2021-07-29 22:01:32 -0400

    Create plotlyfig2dash.m

---
+++ plotlyfig2dash.m
@@ -0,0 +1,6 @@
+function fig = plotlyfig2dash(plotlyfig, id)
+    
+    plotlyfig = py.dict(pyargs('data',plotlyfig.data,'layout',plotlyfig.layout))
+    
+    fig = py.dash_core_components.Graph(pyargs('figure', plotlyfig))
+end

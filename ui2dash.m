a437416e1fec81058ad0e5a441a3837e13dff845
Jack Parmer <jparmer09@gmail.com>
2021-07-30 18:02:47 -0400

    Update ui2dash.m

--- ui2dash.m
+++ ui2dash.m
@@ -1,13 +1,20 @@
 function component = ui2dash(ui_widget, id)
 
-    terminate(pyenv)
-
     switch ui_widget.Type
         case 'uislider'
-            sld = ui_widget
+            
+            sld = ui_widget 
+            
+            slider_marks = py.dict();
+            for i=1:length(sld.MajorTicks)
+                tick = string(sld.MajorTicks(i));
+                update(slider_marks, py.dict(pyargs(tick, tick)));
+            end
+
             component = py.dash_core_components.Slider(pyargs('id',id, ...
                 'min', min(sld.MajorTicks),  ...
                 'max', max(sld.MajorTicks),  ...
                 'value', min(sld.Value),  ...
-                'step', round(max(sld.MajorTicks)/length(sld.MajorTicks))))
+                'marks', slider_marks, ...
+                'step', round(max(sld.MajorTicks)/length(sld.MajorTicks))));
 end

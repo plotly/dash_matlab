148005bc15db98c903ae118170a06b53d6248819
Jack Parmer <jparmer09@gmail.com>
2021-07-30 21:24:09 -0400

    Update dash_app.m

--- dash_app.m
+++ dash_app.m
@@ -12,9 +12,10 @@ app = createApp();
 
 uifig = uifigure('visible', 'off');
 sld = uislider(uifig);
-dash_sld = ui2dash(sld, 'years')
+sld.MajorTicks = [1952,1957,1962,1967,1972,1977,1982,1987,1992,1997,2002,2007];
+dash_sld = ui2dash(sld, 'year')
 
-% create graph
+% create initial graph using fig2plotly
 
 fig = figure('visible', 'off');
 YEAR = 1952;
@@ -25,11 +26,24 @@ scatter(subtab.gdpPercap, ...
     subtab.pop/1000000, ...
     categorical(subtab.country), 'filled');
  set(gca,'xscale','log');
- fig = plotlyfig2dash(fig2plotly(gcf, 'offline', true, 'open', false));
+ fig = plotlyfig2dash(fig2plotly(gcf, 'offline', true, 'open', false), 'graph');
  
 % add slider and graph to Dash app layout
 
-app.layout = addLayout(fig, dash_sld)
+app.layout = addLayout(fig, dash_sld);
+
+% define the callbacks
+
+slider_callback = app.callback( {argsOut('graph', 'figure'), argsIn('year','value') });
+
+% Python callback file
+% Need to turn this Python function into a generic pass through function
+% that calls user-defined MATLAB function through Python matlab.engine
+% utility. Alternatively, if this does not work, could rewrite callback 
+% function entirely in MATLAB: 
+% https://github.com/plotly/dash/blob/dev/dash/dash.py#L990
+
+slider_callback(@py.update_figure.update_figure);  
 
 % run the app
 

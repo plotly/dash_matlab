702308b62b5d979787e2f7c6b4c6e8b237a9259e
igor-gomonov <gomonov@phystech.edu>
2021-08-02 21:43:27 +0300

    commit

--- Examples/slider_scatter/app.m
+++ Examples/slider_scatter/app.m
@@ -6,14 +6,14 @@ tab = readtable('dat.csv');
 
 % create Dash app
 
-app = createApp();
+slider_app = createApp();
 
 % create slider
 
 uifig = uifigure('visible', 'off');
 sld = uislider(uifig);
 sld.MajorTicks = [1952,1957,1962,1967,1972,1977,1982,1987,1992,1997,2002,2007];
-dash_sld = ui2dash(sld, 'year')
+dash_sld = ui2dash(sld, 'year');
 
 % create initial graph using fig2plotly
 
@@ -26,18 +26,19 @@ scatter(subtab.gdpPercap, ...
     subtab.pop/10000, ...
     categorical(subtab.country), 'filled');
  set(gca,'xscale','log');
- fig = plotlyfig2dash(fig2plotly(gcf, 'offline', true, 'open', false,'Visible',false), 'graph');
+ close(fig)
  
+ fig = plotlyfig2dash(fig2plotly(gcf, 'offline', true, 'open', false,'Visible',false), 'graph');
 % add slider and graph to Dash app layout
 
-app.layout = addLayout(fig, dash_sld);
+slider_app.layout = addLayout(fig, dash_sld);
 
 % define the callbacks
 
-slider_callback = app.callback( {argsOut('graph', 'figure'), argsIn('year','value') });  
+slider_callback = slider_app.callback( {argsOut('graph', 'figure'), argsIn('year','value') });  
 
-slider_callback(@py.updatePlotlyGraphPy.updatePlotlyGraphPy);
+slider_callback(@py.callback.callback);
 
 % run the app
 
-app.run_server(pyargs('debug',true,'use_reloader',false,'port','8057'))
+slider_app.run_server(pyargs('debug',true,'use_reloader',false,'port','8057'))
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # dash_matlab
-MATLAB DSL for the Dash Python library
+MATLAB components for the Dash Python library
+Dash for MATLAB provides interface to use Dash library and create analytic web applications fully in MATLAB.
+# Examples
+contains examples of usage:
+- Interactive statistical scatter plot with slider (slider_scatter)  
+...
+...
+# Aux
+Contains backend helper functions
+____
+____
+# Getting Started
+Please be sure to use compatible Python version:  
+https://www.mathworks.com/content/dam/mathworks/mathworks-dot-com/support/sysreq/files/python-compatibility.pdf  
+if your Python version is different, consider downgrading it or using pyenv: https://github.com/pyenv/pyenv  
+____
+Install Dash plotting library: https://github.com/plotly/dash  
+> pip install dash  
+____
+Install Plotly Graphing Library for MATLAB: https://plotly.com/matlab/getting-started/
+Download its sources and install in MATLAB with (don't use setup with URL):  
+> cd ~/Downloads/plotly-graphing-library-for-matlab-master  
+> plotlysetup_offline()  
+____
+Add dash_matlab folder to MATLAB path
+____
+Create insightful graphs in web!
+

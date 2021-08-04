6ca6ecb80eed0b793a8b404aef330a0fedd5c7a0
Carlos Prado <carlosaprado@gmail.com>
2021-08-04 18:46:27 -0300

    Delete Aux Directory

--- Aux/plotlyfig2dash.m
+++
@@ -1,8 +0,0 @@
-function fig = plotlyfig2dash(plotlyfig, id)
-    
-    plotlyfig = py.dict(pyargs('data',plotlyfig.data,'layout',plotlyfig.layout));
-    
-    fig = py.dash_core_components.Loading(pyargs('id','loading-1',...
-        'children',{py.dash_core_components.Graph(pyargs('id', id, 'figure', plotlyfig))},...
-        'type','circle'));
-end

96896400a9bdc9c16781cbfae5256e98e753d0cc
Carlos Prado <carlosaprado@gmail.com>
2021-08-07 19:34:21 -0300

    Update ui2dash.m
    Added common components to ui2dash.m
    
    Table Properties (uitable) is still uncomplete. Continuing with that.

--- helper_functions/ui2dash.m
+++ helper_functions/ui2dash.m
@@ -3,7 +3,7 @@ function component = ui2dash(ui_widget, id)
     switch ui_widget.Type
         case 'uislider'
             
-            sld = ui_widget;
+            sld = ui_widget; 
             
             slider_marks = py.dict();
             for i=1:length(sld.MajorTicks)
@@ -11,13 +11,13 @@ function component = ui2dash(ui_widget, id)
                 update(slider_marks, py.dict(pyargs(tick, tick)));
             end
 
-            component = py.dash_core_components.Slider(pyargs('id', id, ...
-            'min', min(sld.MajorTicks),  ...
-            'max', max(sld.MajorTicks),  ...
-            'value', sld.MajorTicks(1),  ...
-            'marks', slider_marks, ...
-            'step', round(median(diff(sld.MajorTicks)))));
-    
+            component = py.dash_core_components.Slider(pyargs('id',id, ...
+                'min', min(sld.MajorTicks),  ...
+                'max', max(sld.MajorTicks),  ...
+                'value', min(sld.Value),  ...
+                'marks', slider_marks, ...
+                'step', round(max(sld.MajorTicks)/length(sld.MajorTicks))));
+            
         case 'uibuttongroup'
             butgroup = ui_widget;
               
@@ -30,7 +30,8 @@ function component = ui2dash(ui_widget, id)
             'options', opts, ...
             'value', butgroup.Buttons(1).Text, ...
             'labelStyle', py.dict(pyargs('display', 'inline-block')))   );
-           
+        
+        % DropDown Properties
         case 'uidropdown'
             drpdown = ui_widget;
                
@@ -42,13 +43,13 @@ function component = ui2dash(ui_widget, id)
 
             component = py.dash_core_components.Dropdown(pyargs('id', id, ...
             'options', opts, 'value', drpdown.Value ));
-            
+        
         % Button Properties
         case 'uibutton'
             btn = ui_widget;
             component = py.dash_html_components.Button(btn.Text, pyargs(...
                 'id',id, 'n_clicks', 0 ));
-                
+            
         % DatePicker Properties
         case 'uidatepicker'
             dt = ui_widget;
@@ -60,7 +61,7 @@ function component = ui2dash(ui_widget, id)
                 'max_date_allowed',py.datetime.datetime.strptime(date2, '%m/%d/%Y'),...
                 'initial_visible_month',py.datetime.datetime.strptime(date1, '%m/%d/%Y'),...
                 'date',py.datetime.datetime.strptime(date1, '%m/%d/%Y')));
-                
+        
         % CheckBox Properties
         case 'uicheckbox'
             chkbox = ui_widget;
@@ -91,5 +92,34 @@ function component = ui2dash(ui_widget, id)
                 'id', id, ...
                 'value', values, ...
                 'style', py.dict(pyargs('width', '50%', 'height', 150, 'font-size', 14))));
+
+            
+        % Label Properties
+        case 'uilabel'
+            lbl = ui_widget;
+            component = py.dash_html_components.Label(pyargs(...
+                'id',id, 'children', lbl.Text));
+
+        % Hyperlink Properties
+        case 'uihyperlink'
+            hlink = ui_widget;
+            component = py.dash_html_components.Img(pyargs(...
+                'id',id, 'href', hlink.URL, 'target', '_blank', 'children', hlink.Text));
+        
+        % Image Properties
+        case 'uiimage'
+            img = ui_widget;
+            component = py.dash_html_components.Img(pyargs(...
+                'id',id, 'src', img.ImageSource));
+
+        
+        % Table Properties (TODO)
+        case 'uitable'
+            sld = ui_widget
+            component = py.dash_html_components.Table(pyargs(...
+                'children',sld.Data))
+            
+
+            
     end
 end

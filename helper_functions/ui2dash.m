47b661fe81cb7ba6b9428c731d9279e608a096ef
Carlos Prado <carlosaprado@gmail.com>
2021-08-08 03:56:41 -0300

    Update ui2dash.m
    hyperlink should be in dash core components, not in dash html components (fixed)

--- helper_functions/ui2dash.m
+++ helper_functions/ui2dash.m
@@ -103,7 +103,7 @@ function component = ui2dash(ui_widget, id)
         % Hyperlink Properties
         case 'uihyperlink'
             hlink = ui_widget;
-            component = py.dash_html_components.Link(pyargs(...
+            component = py.dash_core_components.Link(pyargs(...
                 'id',id, 'href', hlink.URL, 'target', '_blank', 'children', hlink.Text));
         
         % Image Properties

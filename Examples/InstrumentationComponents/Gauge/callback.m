a6287987e82cad2d0add3af6a192692d6b4daad6
Carlos Prado <carlosaprado@gmail.com>
2021-08-18 18:17:10 -0300

    Add files via upload
    Uploaded examples for Instrumentation Components: gauge, knob and switch.
    
    Matlab components from: https://www.mathworks.com/help/matlab/creating_guis/choose-components-for-your-app-designer-app.html
    
    Dash DAQ Equivalents:
    
    Gauge: https://dash.plotly.com/dash-daq/gauge
    
    Knob: https://dash.plotly.com/dash-daq/knob
    
    Switch: https://dash.plotly.com/dash-daq/toggleswitch

---
+++ Examples/InstrumentationComponents/Gauge/callback.m
@@ -0,0 +1,3 @@
+function val = callback(value)
+    val = jsonencode(value);
+end
---
+++ Examples/InstrumentationComponents/Toggleswitch/app.m
@@ -0,0 +1,29 @@
+terminate(pyenv);
+
+% create Dash app
+switch_app = createApp();
+
+uifig = uifigure('visible', 'off');
+
+% Knob
+s = uiswitch(uifig);
+dash_switch = ui2dash(s, 'my-toggle-switch');
+
+% Output text
+dash_txt = py.dash_html_components.Div('id','toggle-switch-output');
+
+% add switch and text to Dash app layout
+switch_app.layout = addLayout(py.dash_html_components.Div({...
+    dash_switch, dash_txt}));
+
+% define the callbacks
+
+switch_callback = switch_app.callback({...
+    argsOut('toggle-switch-output', 'children'),...
+    argsIn('my-toggle-switch','value')});  
+
+switch_callback(@py.callback.callback);
+
+% run the app
+
+switch_app.run_server(pyargs('debug',true,'use_reloader',false,'port','8057'))

8cc78a1e8e697afb532c361e77c2ad59bf9bf997
Carlos Prado <carlosaprado@gmail.com>
2021-08-25 19:58:25 -0300

    Delete Examples/DataTable directory

--- Examples/DataTable/PythonDrivenFilteringPagingSorting/5-BackendPagingWithFilteringNOTWORKING/callback.py
+++
@@ -1,9 +0,0 @@
-import matlab.engine
-import json
-
-future = matlab.engine.start_matlab(background=True)
-eng = future.result()
-
-def callback(page_current, page_size, filter):
-    df = json.loads(eng.callback(page_current, page_size, sort_by))
-    return [df]

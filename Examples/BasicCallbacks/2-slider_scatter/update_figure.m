5d4efd9e4368602ada5c2d9b7b462b0e36befc13
Carlo <carlosaprado@gmail.com>
2021-10-15 13:45:17 -0300

    fix scatter plots

--- Examples/BasicCallbacks/2-slider_scatter/update_figure.m
+++ Examples/BasicCallbacks/2-slider_scatter/update_figure.m
@@ -1,21 +1,33 @@
 function plotlyFig = update_figure(yearVar)
 
     gapminderDataFiveYear = readtable('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv');
+    continents = unique(gapminderDataFiveYear.continent);
+    colors = 'rgbcmyk';
     
     if yearVar == 0
         yearVar = min(gapminderDataFiveYear.year);
     end
     
     rows = (gapminderDataFiveYear.year == yearVar);
-    subtab = gapminderDataFiveYear(rows,:);
-    scatter(subtab.gdpPercap, ...
-        subtab.lifeExp, ...
-        subtab.pop/100000, ...
-        categorical(subtab.country), 'filled');
+    subtab = gapminderDataFiveYear(rows,:);    
+    close all;
+    hold on;
+    for i=1:length(continents)
+        rowsCont = (strcmp(subtab.continent, continents{i}));
+        subtabCont = subtab(rowsCont,:);
+        
+        scatter(gca, subtabCont.gdpPercap, subtabCont.lifeExp, ...
+            subtabCont.pop/100000, colors(i), 'filled',...
+            'DisplayName', continents{i},...
+            'MarkerFaceAlpha',.8,'MarkerEdgeAlpha',.8);
+    end
+    hold off;
+    
     set(gca,'xscale','log');
     set(gca,'FontSize',14);
     xlabel(gca, 'gdpPercap');
-    ylabel(gca, 'lifeExp');    
+    ylabel(gca, 'lifeExp'); 
+    legend(gca);
 
     fig = fig2plotly(gcf, 'offline', true, 'open', false, 'Visible', false);
     addtheme(fig, 'plotly_dark');
--- Examples/LayoutExamples/LayoutScatter/layoutScatter.m
+++ Examples/LayoutExamples/LayoutScatter/layoutScatter.m
@@ -4,34 +4,30 @@ clearvars; % Removes all variables from the currently active workspace.
 % Read data file
 gdpLifeExp = readtable('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv');
 continents = unique(gdpLifeExp.continent);
-%colors={'red';'blue';'green';'black';'yellow'};
 colors = 'rgbcmyk';
 ax = axes();
-%hold all
+
+hold on;
 for i=1:length(continents)
-    %continents{i}
     rows = (strcmp(gdpLifeExp.continent, continents{i}));
     subtab = gdpLifeExp(rows,:);
 
     scatter(ax, subtab.gdpPerCapita, subtab.lifeExpectancy,...
         subtab.population/100000, colors(i), 'filled',...
         'DisplayName', continents{i});
-    hold on;
-    %legend(ax, continents{i});
 end
 hold off;
 
 set(ax, 'xscale', 'log');
-
 xlabel(ax,'gdp per capita');
 ylabel(ax,'life expectancy');
 legend(ax);
-%legend show
-shg
+
+ax.UserData = struct('theme', 'plotly_dark');
 
 myPlot = ui2dash(ax, 'graph');
 
 components = {myPlot};
 
 % Run the app.
-layoutApp = startDash(components, 8057);
+layoutApp = startDash(components, 8057, [], 'DARKLY');

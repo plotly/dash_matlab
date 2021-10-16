b022b99350806489cb24f4199295a8218184b4ed
Carlo <carlosaprado@gmail.com>
2021-10-16 11:07:29 -0300

    add tableAndGraphExample

--- lib/callbackP.py
+++ lib/callbackP.py
@@ -5,11 +5,11 @@ from dash.exceptions import PreventUpdate
 future = matlab.engine.start_matlab(background=True)
 eng = future.result()
 
-def update_figure(*args):
+def updateGraph(*args):
     if args[0] is None:
-        outputs = json.loads(eng.update_figure("NaN"))
+        outputs = json.loads(eng.updateGraph("NaN"))
     else:
-        outputs = json.loads(eng.update_figure(*args))
+        outputs = json.loads(eng.updateGraph(*args))
     if outputs == 'PreventUpdate':
         raise PreventUpdate
     return outputs
